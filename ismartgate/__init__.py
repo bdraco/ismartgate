6749d0257953dfdc01f6e5f177e45a9ddd170658
J. Nick Koston <nick@koston.org>
2023-01-27 18:23:30 -1000

    Bump version: 4.0.4 → 5.0.0

--- ismartgate/__init__.py
+++ ismartgate/__init__.py
@@ -50,7 +50,7 @@ __author__ = "Robbie Van Gorkom; J. Nick Koston"
 __email__ = "nick@koston.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "4.0.4"
+__version__ = "5.0.0"
 
 
 def get_module_version():
